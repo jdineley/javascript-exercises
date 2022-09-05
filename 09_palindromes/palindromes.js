3e4877154dc369c2497cbe46ef1daa27ad29317d
James Dineley <jdineley@hotmail.com>
2022-09-05 13:36:53 +0100

    Completed palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const replaced = str.replace(/[^a-z]/gi, '').toLowerCase();
+    let arr = replaced.split('')
+    let arrRev = []
+    for(let i = arr.length-1; i >= 0; i--){
+        arrRev.push(arr[i]);
+    }
+    return arr.join('') === arrRev.join('')
 };
 
 // Do not edit below this line
