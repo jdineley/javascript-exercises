ecc6046391797c294c6a26ec7b3d4f887ada1747
James Dineley <jdineley@hotmail.com>
2022-09-06 17:52:13 +0100

    update

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let now = new Date();
+    return people.reduce((acc, cur) => {
+        if(!(acc.yearOfDeath)) acc.yearOfDeath = now.getFullYear();
+        if(!(cur.yearOfDeath)) cur.yearOfDeath = now.getFullYear();
+        if(acc.yearOfDeath && cur.yearOfDeath){
+            if((acc.yearOfDeath - acc.yearOfBirth) > (cur.yearOfDeath - acc.yearOfBirth)){
+                return acc;
+            } else {
+                return cur;
+            }
+        }
+    })   
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
