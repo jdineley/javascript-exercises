ae20e1c705bd99c0fc170fe7338fab7c675c4ffe
James Dineley <jdineley@hotmail.com>
2022-09-06 14:05:26 +0100

    Completed palindromes and fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num > 0){
+        if(num === 1 || num === 2) return 1
+        let arr = [1, 1];
+        while (arr.length < num){
+            let newPush = arr[arr.length-1] + arr[arr.length-2];
+            arr.push(newPush);
+        }
+        return arr[arr.length-1];
+    } else {
+        return 'OOPS'
+    }
 };
 
 // Do not edit below this line
